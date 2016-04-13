7bf0a4f8e108864cdd58783a189ec1aa6425c5b0
David Walker <david.walker@anaplan.com>
2016-04-13 22:22:16 +0100

    224 bytes

--- dave+mel/dojo.py
+++ dave+mel/dojo.py
@@ -3,4 +3,4 @@ a="""{0}, {1}, {2} {3}
 {2} {3}
 """
 s=(a+a+"{4}, {5}, {6} {7}\n"+a).format(*b)
-for p in b:print s;s=s.replace(p,"... ..."if"and"in p else"...")
+for p in b:print s;s=s.replace(p,"... ..."if"nd"in p else"...")
