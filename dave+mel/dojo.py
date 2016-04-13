76ef0da174886f6b352e8c0780893f6f67636f2e
David Walker <david.walker@anaplan.com>
2016-04-13 22:12:44 +0100

    227 bytes

---
+++ dave+mel/dojo.py
@@ -0,0 +1,7 @@
+b="Head~shoulders~knees~and toes~and eyes~and ears~and mouth~and nose".split("~")
+a="""{0}, {1}, {2} {3}
+{2} {3}
+"""
+s=(a+a+"{4}, {5}, {6} {7}\n"+a).format(*b)
+for p in b:
+ print s;s=s.replace(p,"... ..."if"and"in p else"...")
