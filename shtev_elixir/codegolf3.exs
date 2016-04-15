cd6b563b800fcb39e20d87360f681f0ca1f44398
Steve Gilroy <shtev@inspiraldreams.com>
2016-04-15 21:43:05 +0100

    added list comprehension

--- shtev_elixir/codegolf3.exs
+++ shtev_elixir/codegolf3.exs
@@ -1,4 +1,4 @@
 l=List.to_tuple(Enum.zip(1..9,["Head, ","shoulders, ","knees and ","toes","and eyes ","and ears ","and mouth and ", "nose","\n"]))
 p=fn(_,9,_)->"";(i,m,f)->IO.puts(List.foldr(i,"",fn({n,s},a)->if n<=m do String.replace(s,~r/(\w)+/,"...")else s end<>a end)<>if m<8,do: "\n",else: "");f.(i,m+1,f);end
-w=fn(_,[],_)->[];(j,[k|r],y)->[elem(j,k-1)|y.(j,r,y)];end
-p.(w.(l,[1,2,3,4,9,3,4,9,1,2,3,4,9,3,4,9,5,6,7,8,9,1,2,3,4,9,3,4],w),0,p)
+w=for b<-[1,2,3,4,9,3,4,9,1,2,3,4,9,3,4,9,5,6,7,8,9,1,2,3,4,9,3,4], do: elem(l,b-1)
+p.(w,0,p)
