dc177e369ff15ec0dd7a3d4219390e1f9d1a1fcc
Steve Gilroy <shtev@inspiraldreams.com>
2016-04-16 23:56:32 +0100

    removed unecessary parentheses from regex

--- shtev_elixir/codegolf3.exs
+++ shtev_elixir/codegolf3.exs
@@ -1,3 +1,3 @@
 w={"Head, ","shoulders, ","knees and ","toes\n","and eyes ","and ears ","and mouth and ", "nose\n"}
 p=[0,1,2,3,2,3]
-for s<-0..8, do: IO.puts(for m<-p++p++[4,5,6,7]++p do if m<s do String.replace(elem(w,m),~r/(\w)+/,"...")else elem(w,m)end end)
+for s<-0..8, do: IO.puts(for m<-p++p++[4,5,6,7]++p do if m<s do String.replace(elem(w,m),~r/\w+/,"...")else elem(w,m)end end)
